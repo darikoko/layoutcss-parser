ef3cf7b7c85a2b9885db8e69142886fc0cd5a3e3
Loïc <loic@cavinato.fr>
2025-01-21 14:53:56 +0100

    fix LayoutConfig and version

--- src/config.rs
+++ src/config.rs
@@ -4,10 +4,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize)]
 pub struct LayoutStyleConfig {
     pub harmonic_ratio: f64,
-    pub min_screen: String,
-    pub max_screen: String,
     pub base_value: String,
-    pub resizing_ratio: f64,
     pub dev: bool,
 }
 
